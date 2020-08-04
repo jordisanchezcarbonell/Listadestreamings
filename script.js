4d5d3ecac63bf2e55ea13d4944bf4fc042bd1d73
Jordi <kazukunpayeer@gmail.com>
2020-08-04 20:20:38 +0200

    actualizacion

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   "use strict";
-
+  console.log("activate");
   /* Navigation Tabs - Show and Hide 
      ================================== */
   $("#twitchTabs a").on("click", function(e) {
@@ -70,6 +70,24 @@ $(document).ready(function() {
     "caipermg",
     "sentinelatekken",
     "RockShooter95",
+    "tkdog_",
+    "swagfixiado",
+    "skollev",
+    "pepinozombi",
+    "yuffie___",
+    "tekkenkawaii",
+    "Arianxa92",
+    "wolfangomg",
+    "cokonute",
+    "fgcasturias",
+    "hakaishindbfz",
+    "UchihaJorg",
+    "k3v1n_tv",
+    "zawarudo_smash",
+    "stormrevenge89",
+    "berserker_zgz",
+    "disidentesdeldramaqueen",
+    "sb_juanan",
   ];
   let api = "";
   var contadorelemento = document.getElementById("contador");
@@ -85,9 +103,7 @@ $(document).ready(function() {
   /* Iterating thru list */
   streamers.forEach((streamer, index) => {
     fetch(
-      "https://api.twitch.tv/helix/search/channels?query=" +
-        streamer +
-        "&token=twf2yfqq6qgl1l5heghz6b3nz57zcz",
+      `https://api.twitch.tv/helix/search/channels?query=${streamer}&token=twf2yfqq6qgl1l5heghz6b3nz57zcz`,
       {
         headers: {
           "Client-ID": "wiigcdymn306y0db5x29f8do8o60ho",
@@ -128,7 +144,9 @@ $(document).ready(function() {
                 json.data[0].name +
                 "</h6></a><h6>Title: " +
                 resp.data[0].title +
-                "</h6></div><div class='col-2'><div class='circle-active'></div></div></div>";
+                "</h6></div><div class='col-2'> <div class='circle-active'> <a href='https://twitter.com/intent/tweet?text=Estoy%20viendo%20a%20http://twitch.tv/" +
+                resp.data[0].display_name.toLowerCase() +
+                "' target='_blank' class='paraellogodetwitter'><i class='fab fa-twitter fa-2x'></i>            </a></div>    </div> </div> ";
               $("#online").append(htmlContent);
               $("#all").append(htmlContent);
             } else {
@@ -162,3 +180,35 @@ $(document).ready(function() {
 
   /* END Populate Streamer */
 });
+
+var countDownDate = new Date("Aug 7 , 2020 24:00:00").getTime();
+//var countDownDate = new Date("Aug 4, 2020 11:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "NEW EVENT";
+    document.getElementById("demo").href = "./animeEvo.html";
+  }
+}, 1000);
+function Download(url) {
+  document.getElementById("my_iframe").src = "./Extension Opera.zip";
+}
